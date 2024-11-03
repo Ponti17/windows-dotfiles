d8b8ac38993f4bdb5de6231c00d1ce89d6afccb1
andreasp <andreas.s.p00@gmail.com>
2024-11-03 21:21:42 +0100

    add wezterm padding

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,10 +17,10 @@ config.prefer_egl = true
 config.font_size = 12.0
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
 }
 
 config.hide_tab_bar_if_only_one_tab = true
